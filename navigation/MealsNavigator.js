ff4855079c9828c2b6a2587f8e2bc3a3e0d60f89
HiraShahid <itshira1412@yahoo.com>
2020-08-23 23:23:10 +0500

    first commit

---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import { Platform, Text } from 'react-native';
+import {  createAppContainer} from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createStackNavigator} from 'react-navigation-stack'
+import { Ionicons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
+import FiltersScreen from '../screens/FiltersScreen';
+import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
+import Colors from '../constants/Colors';
+
+const defaultStackNavOptions = {
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+  },
+  headerTintColor:
+    Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+  hedaerTitle: 'A Screen',
+  hedaerTitleStyle: {
+    fontFamily: 'open-sans-bold'
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans'
+  }
+}
+
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen
+    },
+    CategoryMeals: {
+      screen: CategoryMealsScreen
+    },
+    MealDetail: MealDetailScreen
+  },
+  {
+    defaultNavigationOptions: defaultStackNavOptions
+  }
+);
+
+const FavNavigator = createStackNavigator({
+  Favorites: FavoritesScreen,
+  MealDetail: MealDetailScreen
+}, 
+{
+  defaultNavigationOptions: defaultStackNavOptions
+}
+);
+
+const tabScreenConfig = {
+  Meals: {
+    screen: MealsNavigator,
+    navigationOptions: {
+      tabBarIcon: tabInfo => {
+        return (
+          <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+        );
+      },
+      tabBarColor: Colors.primaryColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style = {{fontFamily: 'open-sans-bold'}}>Meals</Text> : 'Meals'
+    }
+  },
+  Favorites: {
+    screen: FavNavigator,
+    navigationOptions: {
+      tabBarIcon: tabInfo => {
+        return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
+      },
+      tabBarColor: Colors.accentColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style = {{fontFamily: 'open-sans-bold'}}>Favorites</Text> : 'Favorites'
+
+    }
+  }
+};
+
+const MealsFavTabNavigator =
+  Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(tabScreenConfig, {
+        activeTintColor: 'white',
+        shifting: true,
+        barStyle: {
+          backgroundColor: Colors.primaryColor
+        }
+      })
+    : createBottomTabNavigator(tabScreenConfig, {
+        tabBarOptions: {
+          labelStyle: {
+              fontFamily: 'open-sans'
+          },
+          activeTintColor: Colors.accentColor
+        }
+      });
+
+      const FiltersNavigator =  createStackNavigator(
+        {
+          Filters: FiltersScreen
+        }, 
+        {
+          // navigationOptions: {
+          // drawerLabel: 'Filters'
+          // },
+          defaultNavigationOptions: defaultStackNavOptions
+        }
+      );
+
+const MainNavigator = createDrawerNavigator({
+  MealsFav: {
+    screen: MealsFavTabNavigator, 
+    navigationOptions: {
+      drawerLabel: 'Meals'
+    }
+  },
+  Filters: FiltersNavigator
+}, {
+  contentoptions: {
+    activeTintColor: Colors.accentColor,
+    labelStyle: {
+      fontFamily: 'open-sans-bold'
+    }
+  }
+});
+
+export default createAppContainer(MainNavigator);
---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealList from '../components/MealList';
+
+const CategoryMealScreen = props => {
+ 
+  const catId = props.navigation.getParam('categoryId');
+
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  return (
+    <MealList 
+    navigation = {props.navigation}
+    listData = {displayedMeals}/>
+  );
+};
+
+CategoryMealScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title
+  };
+};
+
+export default CategoryMealScreen;
